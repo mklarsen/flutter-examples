9ee52261c62ba3a688b9070e18778d0378718f6c
Martin Kraus Larsen <26963547+mklarsen@users.noreply.github.com>
2022-03-27 10:23:05 +0200

    init fork

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/martinkl/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/martinkl/github/PUB/flutter-examples"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/martinkl/github/PUB/flutter-examples/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=19.2.44"
+export "FLUTTER_BUILD_NUMBER=19.2.44"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/martinkl/github/PUB/flutter-examples/.dart_tool/package_config.json"
